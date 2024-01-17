1eb536ab8c7a9e891b02b2b937a722ac06137d1b
Katarina-Kovacova <125378069+Katarina-Kovacova@users.noreply.github.com>
2024-01-17 23:26:14 +0000

    Tidy up code  booksearch.py

--- booksearch.py
+++ booksearch.py
@@ -1,111 +1,99 @@
-def loadBooks():
+def load_books():
+    with open('bestsellers_list.txt', 'r') as file:
+        books = file.read()
+        book_list = []
+        books = books.split('\n')
+        for item in books:
+            item_split = item.split('\t')
+            book_list.append(item_split)
 
-	with open('C:/Users/Kate/Desktop/Python/bestsellers/bestsellers_list.txt', 'r') as file:
-		books = file.read()
-		# print(type(books))
-		# print(books)
-		booklist = []
-		books = books.split('\n')
-			# print(books)
-		for item in books:
-			item_split = item.split('\t')
-			booklist.append(item_split)
-		# print(booklist)
+        for item in book_list:
+            item[3] = item[3].split('/')
+        return book_list
 
-		for item in booklist:
-			item[3] = item[3].split('/')
-		return booklist
 
-booklist = loadBooks()
-print(booklist)
-print()	
+def search_by_title(book_list):
+    searched_title = input('Enter the title or part of the title: ')
+    number_of_books_found_by_title = []
+    for item in book_list:
+        title = item[0]
+        if searched_title in title:
+            print(item)
+            number_of_books_found_by_title.append(item)
+    print('Number of books found: ', len(number_of_books_found_by_title))
+    print()
 
-def Choose_search_option():
-	what_do_you_want = ('What would you like to do?')
-	option_one = ('1. Search books by years')
-	option_two = ('2. Search books by month and year')
-	option_three = ('3. Search books by author')
-	option_four = ('4. Search books by title')
-	option_five = ('5. Exit')
-	
-	print(what_do_you_want)
-	print(option_one)
-	print(option_two)
-	print(option_three)
-	print(option_four)
-	print(option_five)
 
-	def searchByTitle():
-		searched_title = input('Enter the title or part of the title: ')
-		number_of_books_found_by_title = []
-		for item in booklist:
-			title = item[0]
-			if searched_title in title:
-				print(item)
-				number_of_books_found_by_title.append(item)
-		print('Number of books found: ', len(number_of_books_found_by_title))
-		print()	
-		Choose_search_option()
+def search_by_author(book_list):
+    searched_author = input('Enter the author name or part of the author name: ')
+    number_of_books_found_by_author = []
+    for item in book_list:
+        author = item[1]
+        if searched_author in author:
+            print(item)
+            number_of_books_found_by_author.append(item)
+    print('Number of books found: ', len(number_of_books_found_by_author))
+    print()
 
-	def searchByAuthor():
-		searched_author = input('Enter the author name or part of author name: ')
-		number_of_books_found_by_author = []
-		for item in booklist:
-			author = item[1]
-			if searched_author in author:
-				print(item)
-				number_of_books_found_by_author.append(item)
-		print('Number of books found: ', len(number_of_books_found_by_author))
-		print()		
-		Choose_search_option()
 
-	def searchByYears():
-		start_year = input('Enter start year: ')
-		end_year = input('Enter end year: ')
-		number_of_books_found_by_year = []
-		for item in booklist:
-			year = item[3][2]
-			if start_year <= year <= end_year:
-				print(item)
-				number_of_books_found_by_year.append(item)
-		print('Number of books found: ', len(number_of_books_found_by_year))
-		print()	
-		Choose_search_option()
+def search_by_years(book_list):
+    start_year = input('Enter start year: ')
+    end_year = input('Enter end year: ')
+    number_of_books_found_by_year = []
+    for item in book_list:
+        year = item[3][2]
+        if start_year <= year <= end_year:
+            print(item)
+            number_of_books_found_by_year.append(item)
+    print('Number of books found: ', len(number_of_books_found_by_year))
+    print()
 
-	def searchByMonthAndYear():
-		user_month = input('Enter month as number 1-12: ')
-		user_year = input ('Enter year: ')
-		number_of_books_found_by_month_and_year = []
-		for item in booklist:
-			month = item[3][0]
-			year = item [3][2]
-			if user_month == month and user_year == year:
-				print(item)
-				number_of_books_found_by_month_and_year.append(item)
-		print('Number of books found: ', len(number_of_books_found_by_month_and_year))
-		print()	
-		Choose_search_option()
 
-	def exit():
-		print('Thank you for using my first ever programme! Bye :-) ')
-		quit()
-		
-	user_selection = int(input('Enter number between 1-5: '))
-	if user_selection == 1:
-		searchByYears()
+def search_by_month_and_year(book_list):
+    user_month = input('Enter month as a number (1-12): ')
+    user_year = input('Enter year: ')
+    number_of_books_found_by_month_and_year = []
+    for item in book_list:
+        month = item[3][0]
+        year = item[3][2]
+        if user_month == month and user_year == year:
+            print(item)
+            number_of_books_found_by_month_and_year.append(item)
+    print('Number of books found: ', len(number_of_books_found_by_month_and_year))
+    print()
 
-	if user_selection == 2:
-		searchByMonthAndYear()
 
-	if user_selection == 3:
-		searchByAuthor()
+def exit_program():
+    print('Thank you for using my program! Bye :-) ')
+    quit()
 
-	if user_selection == 4:
-		searchByTitle()
 
-	if user_selection == 5:
-		exit()
-	# return?	
+book_list = load_books()
+print()
 
-Choose_search_option()
+while True:
+    menu = '''
+    What would you like to do?
+    1. Search books by year
+    2. Search books by month and year
+    3. Search books by author
+    4. Search books by title
+    5. Exit
+    '''
 
+    print(menu)
+
+    user_selection = input('Enter a number between 1-5: ')
+
+    if user_selection == '1':
+        search_by_years(book_list)
+    elif user_selection == '2':
+        search_by_month_and_year(book_list)
+    elif user_selection == '3':
+        search_by_author(book_list)
+    elif user_selection == '4':
+        search_by_title(book_list)
+    elif user_selection == '5':
+        exit_program()
+    else:
+        print('Invalid option. Please choose a number between 1 and 5.')
